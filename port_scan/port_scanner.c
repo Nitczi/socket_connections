3b45c3f6ec05b0c8ca779ae4af0d3d88e269a031
Nitczi <51859779+Nitczi@users.noreply.github.com>
2023-08-21 22:33:49 -0300

    Create port_scanner.c
    This is like a 1.1 version of msocket.c program.
    
    It's the same code but with a for loop implemented.

---
+++ port_scan/port_scanner.c
@@ -0,0 +1,48 @@
+// A simple portscanner made by me, Nitczi.
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+
+int main(int argc, char* argv[]){
+
+   if(argc<2){
+      printf("Use mode incorret\n");
+      printf("./port_scanner target")
+      return 1;
+   }
+
+   int mysock;
+   int connection;
+   int port_in = 1;
+   int port_fin = 65535;
+   char *targ;
+   targ = argv[1];
+   
+   struct sockaddr_in address;
+
+   for(int port=port_in; port<port_fin; port++){
+      mysock = socket(AF_INET, SOCK_STREAM, 0);
+      address.sin_family = AF_INET;
+      address.sin_port = htons(port);
+      address.sin_addr.s_addr = inet_addr(targ); 
+
+      connection = connect(mysock, (struct sockaddr *)&address, sizeof(address));
+   
+      if( connection == 0 ){
+         printf("PORT OPEN: %i\n", port);
+         close(mysock);
+         close(connection);  
+      } else {
+         close(mysock);
+         close(connection);
+      }
+
+   }
+
+   close(mysock);
+   close(connection);
+
+   return 0;
+}
